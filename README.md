f92290c888c24ca11950c5fce762693795dc0d5b
dsxsteven <dsxsteven@sina.com>
2026-05-14 15:32:06 +0800

    Update README & fix template config.py
    Signed-off-by: dsxsteven <dsxsteven@sina.com>

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+原链接参考
+https://wiki.huawei.com/domains/143914/wiki/137614/WIKI202511048896614
+
 `使用前请先下载最新版本的 `aisbench`，请按以下步骤**彻底卸载**镜像自带的旧版本，并重新从源码安装。`
 
 ## 卸载旧版本（彻底清除）
@@ -15,3 +18,34 @@ git clone https://github.com/AISBench/benchmark.git
 pip3 install -e ./ --use-pep517 -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip3 install -r requirements/api.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip3 install -r requirements/extra.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+```
+
+## 新增变长参数：不输入及使用原来的定长测试
+
+分别为：
+输入长度均值、输入长度标准差、输入长度最小值、输入长度最大值
+
+```
+length_mean: Optional[int] = None,
+length_std: Optional[float] = None,   
+length_min: Optional[int] = None,
+length_max: Optional[int] = None,
+```
+
+变长数据生成网站：
+https://www.bchrt.com/tools/normal-distribution-generator/
+
+### 参考变长aisbench样例：
+
+输入8k\~128k不定长，平均32k；输出300
+
+```
+python3 aisbench_test.py --input_len 32768 --output_len 300 --data_num 32 --concurrency 8 --request_rate 0 --dataset_type prefix_cache --repeat_rate 90% --prefix_test --dp_size 2 --length_mean 32768 --length_std 49152 --length_min 8192 --length_max 131072
+```
+
+
+## 常见问题
+
+1. 结果被重定向到了aisbench打屏中，**不会存到aisbench.log**，如有需要请自行保存log
+2. 请注意修改config.py中的`WORK_PATH`，**不再是镜像usr中的默认路径**
+3. 请在aisbench_auto_tools_prefix-master-pro路径下执行指令，以免找不到文件
--- config.py
+++ config.py
@@ -1,73 +1,19 @@
-# # 数据集文件夹
-# DATASET_PATH = "/mnt/share/"
-# # aisbench 工作路径, 为 git clone aisbench 后得到的 benchmark 目录的绝对路径
-# # 可通过命令 `pip show ais-bench-benchmark | grep location` 查询
-# WORK_PATH = "/usr/local/python3.11.10/lib/python3.11/site-packages"
-# # 服务化配置的模型名称
-# MODEL_NAME = "glm-5"
-# # 模型权重路径, 用于读取 tokenizer
-# # MODEL_PATH = "/mnt/share/w30061490/weights/glm5-w8a8"
-# MODEL_PATH = "/mnt/share/q00946761/weights/DeepSeek-V2-Lite-Chat"
-# # 请求目的 IP
-# HOST_IP = "141.61.81.154"
-# # 请求目的端口
-# HOST_PORT = "6060"
+# 存放数据集的文件夹路径，需可访问
+DATASET_PATH = "/mnt/path_to_store_dataset"
 
-# # 如果使用稳态测试请将该字段设置为 "stable_stage"
-# DEFAULT_PERFORMANCE_TEST = "default_perf"
-
-
-
-# 数据集文件夹
-DATASET_PATH = "/mnt/share/"
 # aisbench 工作路径, 为 git clone aisbench 后得到的 benchmark 目录的绝对路径
-# 可通过命令 `pip show ais-bench-benchmark | grep location` 查询
-WORK_PATH = "/home/l00889328/aisbench/benchmark"
+# 可通过命令 `pip show ais_bench_benchmark | grep location` 查询
+# mindie镜像打包的aisbench位置为 /usr/local/lib/python3.11/site-packages
+WORK_PATH = "/usr/local/lib/python3.11/site-packages"
+
 # 服务化配置的模型名称
-MODEL_NAME = "glm-5"
+MODEL_NAME = "ds_r1"
 # 模型权重路径, 用于读取 tokenizer
-# MODEL_PATH = "/mnt/share/w30061490/weights/glm5-w8a8"
-MODEL_PATH = "/mnt/share/q00946761/weights/DeepSeek-V2-Lite-Chat"
+MODEL_PATH = "/mnt/weight/DeepSeek-R1_w8a8_mtp"
 # 请求目的 IP
-HOST_IP = "141.61.81.154"
+HOST_IP = "141.1.1.101"
 # 请求目的端口
-HOST_PORT = "7060"
+HOST_PORT = "31015"
 
 # 如果使用稳态测试请将该字段设置为 "stable_stage"
 DEFAULT_PERFORMANCE_TEST = "default_perf"
-
-
-# # 数据集文件夹
-# DATASET_PATH = "/mnt/share/"
-# # aisbench 工作路径, 为 git clone aisbench 后得到的 benchmark 目录的绝对路径
-# # 可通过命令 `pip show ais-bench-benchmark | grep location` 查询
-# WORK_PATH = "/usr/local/python3.11.10/lib/python3.11/site-packages"
-# # 服务化配置的模型名称
-# MODEL_NAME = "glm-5"
-# # 模型权重路径, 用于读取 tokenizer
-# MODEL_PATH = "/mnt/share/l00899543/kimi25_w4a8_static_m6"
-# # 请求目的 IP
-# HOST_IP = "141.61.81.154"
-# # 请求目的端口
-# HOST_PORT = "8000"
-
-# # 如果使用稳态测试请将该字段设置为 "stable_stage"
-# DEFAULT_PERFORMANCE_TEST = "default_perf"
-
-
-# # 数据集文件夹
-# DATASET_PATH = "/mnt/share/"
-# # aisbench 工作路径, 为 git clone aisbench 后得到的 benchmark 目录的绝对路径
-# # 可通过命令 `pip show ais-bench-benchmark | grep location` 查询
-# WORK_PATH = "/usr/local/python3.11.10/lib/python3.11/site-packages"
-# # 服务化配置的模型名称
-# MODEL_NAME = "glm-5"
-# # 模型权重路径, 用于读取 tokenizer
-# MODEL_PATH = "/mnt/share/l00899543/kimi25_w4a8_static_m6"
-# # 请求目的 IP
-# HOST_IP = "141.61.81.154"
-# # 请求目的端口
-# HOST_PORT = "8001"
-
-# # 如果使用稳态测试请将该字段设置为 "stable_stage"
-# DEFAULT_PERFORMANCE_TEST = "default_perf"
